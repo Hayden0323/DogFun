fb80541de2cbf1cd45a446950224d1f5caa471f0
Hayden <hayden@HaydendeMacBook-Pro.local>
2020-01-31 19:53:15 +0800

    后台界面搭建 视频展示页 2020/1/31

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 import './plugins/element'
+import EleForm from 'vue-ele-form'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(EleForm)
+Vue.prototype.$http = axios.create({
+  baseURL: 'http://localhost:3000'
+})
 
 new Vue({
   router,
---
+++ admin/src/views/videos/VideoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h3>视频列表</h3>
+    <el-table :data="data.data"
+              border
+              stripe>
+      <el-table-column v-for="(field,name) in fields"
+                       :prop="name"
+                       :key="name"
+                       :label="field.label">
+      </el-table-column>
+    </el-table>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class VideoList extends Vue {
+  data = {};
+  fields = {
+    _id: { label: "ID" },
+    name: { label: "视频名称" },
+    cover: { label: "视频封面图" }
+  };
+
+  async fetch() {
+    const res = await this.$http.get("videos");
+    this.data = res.data;
+  }
+
+  created() {
+    this.fetch();
+  }
+}
+</script>
+
+<style>
+</style>
