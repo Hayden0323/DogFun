0c8078864c7c01ac1400e639141cfa8d282052df
Hayden <hayden@HaydendeMacBook-Pro.local>
2020-02-07 19:22:24 +0800

    web端实现视频页和分集页 2020/2/7

--- server/apps/server/src/app.module.ts
+++ server/apps/server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { CommonModule } from '@app/common'
+import { VideosModule } from './videos/videos.module'
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, VideosModule],
   controllers: [AppController],
   providers: [AppService]
 })
