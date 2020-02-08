0f0a01de7e19939bc51f4b235d7cf5632d471ee1
Hayden <hayden@HaydendeMacBook-Pro.local>
2020-02-08 19:03:41 +0800

    实现客户端用户登录注册策略 2020/2/8

--- server/apps/server/src/main.ts
+++ server/apps/server/src/main.ts
@@ -7,10 +7,11 @@ async function bootstrap() {
   app.enableCors()
 
   const options = new DocumentBuilder()
-    .setTitle('DogFun-后台管理API')
-    .setDescription('供后台管理界面调用的服务端API')
+    .setTitle('DogFun-前端管理API')
+    .setDescription('供网站和APP调用的服务端API')
     .setVersion('1.0')
     .addTag('dogs')
+    .addBearerAuth()
     .build()
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('api-docs', app, document)
