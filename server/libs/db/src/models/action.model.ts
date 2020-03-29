ae1328d6615eefa66ddec1fa992c90a76a8364cf
Hayden <hayden@HaydendeMacBook-Pro.local>
2020-03-29 15:31:30 +0800

    添加订阅模型 2020/3/29

---
+++ server/libs/db/src/models/action.model.ts
@@ -0,0 +1,23 @@
+import { prop, modelOptions, Ref } from '@typegoose/typegoose'
+import { User } from './user.model'
+import { Video } from './video.model'
+import { Episode } from './episode.model'
+
+@modelOptions({
+  schemaOptions: {
+    timestamps: true
+  }
+})
+export class Action {
+  @prop({ ref: 'User' })
+  user: Ref<User>
+
+  @prop({ enum: ['Video', 'Episode'] })
+  type: String
+
+  @prop({ refPath: 'type' })
+  object: Ref<Video | Episode>
+
+  @prop({ enum: ['like', 'upVote'] })
+  name: String
+}
