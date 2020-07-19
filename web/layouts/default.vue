d8d85b4b2a3833102f487cbb689942926ec640a5
Hayden <haosong0323@gmail.com>
2020-07-19 17:01:31 +0800

    chore: 页面调整主题

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -54,23 +54,28 @@
 
     <v-app-bar app
                clipped-left
-               color="red"
-               dense>
+               dense
+               flat>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-icon class="mx-4">fab fa-youtube</v-icon>
+      <v-icon class="mx-4 blue--text">pets</v-icon>
       <v-toolbar-title class="mr-12 align-center">
-        <span class="title">DOGFUN</span>
+        <span class="subtitle-1 font-weight-bold">DOGFUN</span>
       </v-toolbar-title>
       <v-spacer />
       <v-row align="center"
-             style="max-width: 650px">
-        <v-text-field :append-icon-cb="() => {}"
-                      placeholder="搜索..."
+             style="max-width: 30vw">
+        <v-text-field placeholder="搜索..."
                       single-line
+                      filled
+                      rounded
+                      dense
                       append-icon="search"
                       color="white"
                       hide-details />
       </v-row>
+      <v-spacer />
+      <v-switch v-model="$vuetify.theme.dark"
+                hide-details></v-switch>
     </v-app-bar>
 
     <v-content>
@@ -146,7 +151,7 @@ export default {
     }
   },
   created () {
-    this.$vuetify.theme.dark = true
+    this.$vuetify.theme.dark = false
   },
 }
 </script>
